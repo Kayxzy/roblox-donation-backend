be43eb9a109d7878c3f00682c38d8d326b392288
kay-ex <106707683+Kayxzy@users.noreply.github.com>
2026-03-09 02:03:49 +0700

    Set up Express server with CORS and routes

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require("express")
+const cors = require("cors")
 
+const config = require("./config")
+const donationRoutes = require("./routes/donation")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/sociabuzz", donationRoutes)
+
+app.listen(config.SERVER.PORT, () => {
+  console.log("Server running on port", config.SERVER.PORT)
+})
