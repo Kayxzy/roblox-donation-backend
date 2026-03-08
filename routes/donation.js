cfc652f8eda1bcf9bf141a924386dc1146aa4af8
kay-ex <106707683+Kayxzy@users.noreply.github.com>
2026-03-09 02:04:14 +0700

    Update donation.js

--- routes/donation.js
+++ routes/donation.js
@@ -1,1 +1,20 @@
+const express = require("express")
+const router = express.Router()
 
+const fs = require("fs")
+const path = require("path")
+
+const DB = path.join(__dirname,"../database/donations.json")
+
+router.get("/get-donations",(req,res)=>{
+
+  const data = JSON.parse(fs.readFileSync(DB))
+
+  res.json({
+    success:true,
+    donations:data
+  })
+
+})
+
+module.exports = router
